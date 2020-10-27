2a991b4cd7e7736f3f0d65c265ac906997eddbbd
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2020-10-27 07:16:10 +0530

    Create Number_Of_Recent_Calls.py

---
+++ Number_Of_Recent_Calls.py
@@ -0,0 +1,20 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.slide_window = deque()
+        
+
+    def ping(self, t: int) -> int:
+        #1. Append the current call
+        self.slide_window.append(t)
+        
+        #2. Invalidate the outdated pings
+        while self.slide_window[0] < t - 3000:
+            self.slide_window.popleft()
+            
+        return len(self.slide_window)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
