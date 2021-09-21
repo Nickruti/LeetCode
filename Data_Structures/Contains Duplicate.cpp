1e8baceaa49f1b8ca572bb8fbcfa6a1078e741bb
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2021-09-21 22:44:37 +0530

    Update Contains Duplicate.cpp

--- Data_Structures/Contains Duplicate.cpp
+++ Data_Structures/Contains Duplicate.cpp
@@ -8,3 +8,23 @@ public:
         return nums.size() > set<int> (nums.begin(), nums.end()).size();  
     }
 };
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map <int, int> m;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.count(nums[i]))
+            {
+                return true;
+            }
+            m[nums[i]] = i;
+        }
+        
+        return false;
+    }
+    
+};
