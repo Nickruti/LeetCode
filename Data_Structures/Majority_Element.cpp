d7e5814759c137ab54f55fc69fbe97aa986fd894
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2021-10-19 22:55:21 +0530

    Create Majority_Element.cpp

---
+++ Data_Structures/Majority_Element.cpp
@@ -0,0 +1,28 @@
+/*
+url - https://leetcode.com/problems/majority-element/
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map <int, int> m;
+        int n = nums.size();
+        
+        for(int i = 0; i< n; i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        int max = INT_MIN, ans;
+        for(auto it = m.begin(); it != m.end(); it++)
+        {
+            if (it->second > max)
+            {
+                max = it->second;
+                ans = it->first;
+            }
+        }
+            
+        return ans;
+    }
+};
