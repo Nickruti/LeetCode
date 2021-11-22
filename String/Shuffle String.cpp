66c8422745e1b3153e5029e43a570b66d3e396b2
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2021-11-22 23:03:01 +0530

    Create Shuffle String.cpp

---
+++ String/Shuffle String.cpp
@@ -0,0 +1,16 @@
+// url - https://leetcode.com/problems/shuffle-string/
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        
+        string ans(s.length(), '*');
+        
+        for (int i = 0; i < indices.size(); i++)
+        {
+            ans[indices[i]] = s[i];
+        }
+        return ans;
+        
+    }
+};
