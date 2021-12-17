4bcec26dbcca13cf477d1929bfd9568cb9425c2f
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2021-12-17 08:28:58 +0530

    Create Length of Last Word.cpp

---
+++ String/Length of Last Word.cpp
@@ -0,0 +1,12 @@
+// url -> 
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        
+        while(ss >> word){}
+        
+        return word.length();
+    }
+};
