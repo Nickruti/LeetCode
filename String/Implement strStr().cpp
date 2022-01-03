dbecd145c51027f5dbebc79cd1eaff966b8b70b7
Kruti Patel <37500239+Nickruti@users.noreply.github.com>
2022-01-03 08:40:55 +0530

    Update Implement strStr().cpp

--- String/Implement strStr().cpp
+++ String/Implement strStr().cpp
@@ -31,3 +31,70 @@ public:
        return ans; 
     }
 };
+
+// Url - https://leetcode.com/problems/implement-strstr/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle == "") return 0;
+        
+        int n = needle.length();
+        
+        int lps[n];
+        lps[0] = 0;
+        int len = 0;
+        
+        int i = 1;
+        while(i < n)
+        {
+            if (needle[i] == needle[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+                }   
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }       
+            }
+            
+        }
+        
+        int j = 0;
+        i = 0;
+        
+        while (i < haystack.length())
+        {
+            if (haystack[i] == needle[j])
+            {
+                i++;
+                j++;
+            }
+            if (j == n)
+            {
+                return (i - j);
+            }
+            else if (haystack[i] != needle[j])
+            {
+                if (j > 0)
+                {
+                    j = lps[j - 1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+};
