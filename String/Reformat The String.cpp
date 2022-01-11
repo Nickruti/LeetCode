9df45ebc3d2c94284d1d6af7b47aff4ce87c0cb4
Nickruti <patelnicky66@gmail.com>
2022-01-11 09:50:02 +0530

    New Problem Solved

---
+++ String/Reformat The String.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    string reformat(string s) {
+        if (s.length() <= 1) return s;
+        
+        vector<int> numbers;
+        vector<char> characters;
+        string ans = "";
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isalpha(s[i]))
+            {
+                characters.push_back(s[i]);
+            }
+            else if (isdigit(s[i]))
+            {
+                numbers.push_back(s[i]);
+            }
+        }
+        
+        int n = numbers.size(), c = characters.size(), i = 0;
+        if ( n != 0 && c != 0 )
+        {
+            if (n > c)
+            {
+                ans.push_back(numbers[0]);
+                while((i != n) && (i != c))
+                {
+                    ans.push_back(characters[i]);
+                    i++;
+                    ans.push_back(numbers[i]);
+                }
+            }
+            else if (c > n)
+            {
+                ans.push_back(characters[0]);
+                while((i != n) && (i != c))
+                {
+                    ans.push_back(numbers[i]);
+                    i++;
+                    ans.push_back(characters[i]);
+                }
+            }
+            else
+            {
+                while((i != n) && (i != c))
+                {
+                    ans.push_back(numbers[i]);
+                    ans.push_back(characters[i]);
+                    i++;
+                }
+            }
+            
+            if (i == n && (i + 1) < c || i == c && (i + 1) < n)
+            {
+                return "";
+            }
+            else
+            {
+                return ans;
+            }
+            
+        }
+        return ans;
+    }
+};
