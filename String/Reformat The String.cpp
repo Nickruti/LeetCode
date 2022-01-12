9d80a84ef04c90c8dfde879a6c061c1d803fdcd5
Nickruti <patelnicky66@gmail.com>
2022-01-12 08:55:39 +0530

    Solved a new Problem

--- String/Reformat The String.cpp
+++ String/Reformat The String.cpp
@@ -65,3 +65,59 @@ public:
         return ans;
     }
 };
+
+// A Better Solution
+
+class Solution {
+public:
+    string reformat(string s) {
+        if (s.length() <= 1) return s;
+        
+        vector<int> numbers;
+        vector<char> characters;
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (isalpha(s[i]))
+            {
+                characters.push_back(s[i]);
+            }
+            else if (isdigit(s[i]))
+            {
+                numbers.push_back(s[i]);
+            }
+        }
+        
+        int n = numbers.size(), c = characters.size();
+        string ans = "";
+        
+        if (abs(n - c) > 1)   return ans;
+        
+        int i = 0;
+        if (n > c)
+        {
+            ans.push_back(numbers[0]);
+            while(i != c)
+            {
+                ans.push_back(characters[i]);
+                i++;
+                ans.push_back(numbers[i]);
+            }
+        }
+        else
+        {
+            while(i != n)
+            {
+                ans.push_back(characters[i]);
+                ans.push_back(numbers[i]);
+                i++;
+            }
+            if (c > n)
+            {
+                ans.push_back(characters[i]);
+            }
+        }
+
+        return ans;      
+    }
+};
