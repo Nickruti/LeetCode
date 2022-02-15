c5577a68114b5973c6c4ba3a840bd78982856db1
Nickruti <patelnicky66@gmail.com>
2022-02-15 23:01:48 +0530

    Solved new Problem

--- String/Generate Parentheses.cpp
+++ String/Generate Parentheses.cpp
@@ -36,3 +36,43 @@ public:
         return dp[n-1];
     }
 };
+
+// Constructor Solution
+class Solution {
+public:
+    vector<vector<string>> dp = {{"()"}};
+    
+    Solution()
+    {
+        for(int l = 1; l < 9; l++)
+        {
+            set<string> temp;
+            for(int i = 0; i < dp[l-1].size(); i++)
+            {
+                temp.insert("(" + dp[l-1][i] + ")");
+                temp.insert("()" + dp[l-1][i]);
+                temp.insert(dp[l-1][i] + "()");
+            }
+
+            for(int k = 1; k < (l + 1)/2; k++)
+            {
+                for(int i = 0; i < dp[l-k-1].size(); i++)
+                {  
+                    for(int j = 0; j < dp[k].size(); j++)
+                    {
+                        temp.insert(dp[l-k-1][i] + dp[k][j]);
+                        temp.insert(dp[k][j] + dp[l-k-1][i]);
+                        
+                    }
+                }
+            }
+              
+            vector<string> q(temp.begin(), temp.end());
+            dp.push_back(q);  
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        return dp[n-1];
+    }
+};
