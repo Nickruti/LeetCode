3002d1eb2ea841528e64231b43ad243e29eb4fad
Nickruti <patelnicky66@gmail.com>
2022-04-14 09:40:47 +0530

    Solved a Math Problem - 202

---
+++ Math/202. Happy Number.cpp
@@ -0,0 +1,20 @@
+// Url - https://leetcode.com/problems/happy-number/
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        while(n != 1 && n != 4)
+        {
+            int sum = 0;
+            while(n > 0)
+            {
+                sum += pow((n % 10), 2);
+                n = n/10;
+            }
+            n = sum;
+        }
+        
+        return n == 1;
+    }
+};
