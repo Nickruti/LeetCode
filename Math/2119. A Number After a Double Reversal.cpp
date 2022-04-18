4e9bc71fe458c967428edc2b76cc6a4b6fa84384
Nickruti <patelnicky66@gmail.com>
2022-04-18 09:42:29 +0530

    Solved a Math Problem - 2119

---
+++ Math/2119. A Number After a Double Reversal.cpp
@@ -0,0 +1,8 @@
+// Url - https://leetcode.com/problems/a-number-after-a-double-reversal/
+
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        return num == 0 || num % 10 > 0;
+    }
+};
